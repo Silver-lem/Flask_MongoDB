76617829801a244d76678bf27206a5197626239a
Geethika <april.appy24@gmail.com>
2025-12-30 01:42:57 +0530

    Fetching data

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__) #An instance of flask class
 
-@app.route("/")
+@app.route('/')
 def home():
     return "<h1>Hello World</h1>"
+
+@app.route('/<name>')
+def second(name):
+    return "Hello " + name
+
+@app.route('/<a>/<b>')
+def third(a,b):
+    sum = int(a) + int(b) #using int() to typecast since by default it is considered to be a string
+
+    result = {
+        'ans' : sum
+    }
+
+    # return str(result)
+    return result
+
+@app.route('/api')
+def func():
+    name = request.values.get('name')
+    age = request.values.get('age')
+
+    result = {
+        'name' : name,
+        'age' : age
+    }
+
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
