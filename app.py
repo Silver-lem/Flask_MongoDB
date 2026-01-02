577e6214244c1ca2284badf453126e9cf41ad5df
Geethika <april.appy24@gmail.com>
2026-01-03 01:27:48 +0530

    MongoDB Atlas Connections made

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from flask import Flask,request,render_template
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+MONGO_URL = os.getenv('MONOGO_URL')
+
+uri = "MONGO_URL"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__) #An instance of flask class
 
@@ -19,9 +39,12 @@ def time():
 
 @app.route('/submit',methods=['POST'])
 def submit():
-    name = request.form.get('name')
+    # name = request.form.get('name')
+    # return 'Hello, ' + name + '!'
+    form_data = dict(request.form)
 
-    return 'Hello, ' + name + '!'
+    print(form_data)
+    return form_data
 
 if __name__ == '__main__':
     app.run(debug=True)
