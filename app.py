f425da270e7a3209ef71ad9439e7eacb24c99c94
Geethika <april.appy24@gmail.com>
2026-01-02 23:17:40 +0530

    Dynamic template added

--- app.py
+++ app.py
@@ -1,39 +1,21 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
+from datetime import datetime
 
 app = Flask(__name__) #An instance of flask class
 
 @app.route('/')
 def home():
-    return "<h1>Hello World</h1>"
+    day_of_week = datetime.today().strftime('%A')
+    current_time = datetime.now().strftime('%H:%M:%S')
+    # print(day_of_week)
+    return render_template('index.html',day_of_the_week=day_of_week,current_time=current_time)
 
-@app.route('/<name>')
-def second(name):
-    return "Hello " + name
 
-@app.route('/<a>/<b>')
-def third(a,b):
-    sum = int(a) + int(b) #using int() to typecast since by default it is considered to be a string
+@app.route('/time')
+def time():
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return current_time
 
-    result = {
-        'ans' : sum
-    }
-
-    # return str(result)
-    return result
-
-@app.route('/api')
-def func():
-    name = request.values.get('name')
-    age = request.values.get('age')
-
-    result = {
-        'name' : name,
-        'age' : age
-    }
-
-    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
