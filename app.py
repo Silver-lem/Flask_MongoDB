c808af0b53a80f828d644fb4c949ec66854b7c03
Geethika <april.appy24@gmail.com>
2026-01-02 23:39:09 +0530

    frontend - backend

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def time():
     return current_time
 
 
+@app.route('/submit',methods=['POST'])
+def submit():
+    name = request.form.get('name')
+
+    return 'Hello, ' + name + '!'
+
 if __name__ == '__main__':
     app.run(debug=True)
