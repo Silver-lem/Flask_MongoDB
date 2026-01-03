f3b07d2be369eda02368010544f2c99f56f13473
Geethika <april.appy24@gmail.com>
2026-01-03 22:43:03 +0530

    Re-adding Mongo Atlas

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask,request,render_template
 from datetime import datetime
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "MONOGO_URL"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__) #An instance of flask class
 
