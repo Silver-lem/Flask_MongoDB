12008aa63786beafb6bb1f195a4d441a6dfeca6f
Geethika <april.appy24@gmail.com>
2026-01-03 22:56:20 +0530

    Revert "MongoDB Atlas Connections made"
    This reverts commit 577e6214244c1ca2284badf453126e9cf41ad5df.

--- app.py
+++ app.py
@@ -1,30 +1,6 @@
 from flask import Flask,request,render_template
 from datetime import datetime
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from dotenv import load_dotenv 
-import os
 
-load_dotenv()
-MONGO_URI = os.getenv('MONGO_URI')
-
-uri = MONGO_URI
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-db = client.test
-collection = db['flask_test']
-
-#####################################################################################################
 
 app = Flask(__name__) #An instance of flask class
 
@@ -44,10 +20,12 @@ def time():
 
 @app.route('/submit',methods=['POST'])
 def submit():
-    name = request.form.get('name')
-    
+    # name = request.form.get('name')
+    # return 'Hello, ' + name + '!'
+    form_data = dict(request.form)
 
-    return 'Hello, ' + name + '!'
+    print(form_data)
+    return form_data
 
 if __name__ == '__main__':
     app.run(debug=True)
